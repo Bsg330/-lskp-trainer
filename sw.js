0597a116f9d342a6e26b527a900a1a5d5e267e4a
Bsg330 <svenwillumeit@gmx.de>
2026-09-21 03:04:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='lskp01',A=['./','index.html','style.css','app.js','questions.json','icon.png','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
